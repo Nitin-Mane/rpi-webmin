b7f7b4a06ddf43daae464aeb52e3259897688b79
purpleax <guybrown@outlook.com>
2019-04-05 17:00:21 +1100

    Create webmin-install.sh

---
+++ webmin-install.sh
@@ -0,0 +1,15 @@
+
+#Update core OS
+sudo apt-get update
+sudo apt-get upgrade
+
+#Install the pre-reqs
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+
+#Get the Webmin Package
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.890_all.deb
+
+#Install the package
+sudo dpkg --install webmin_1.890_all.deb
+
+echo "The Webmin console is accessible from the local IP address on port 10000"
