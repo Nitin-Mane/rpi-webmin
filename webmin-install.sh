f03c4935121eaff8b031aa7b73c8745aaf36e056
purpleax <guybrown@outlook.com>
2019-04-12 15:24:41 +1000

    Update webmin-install.sh

--- webmin-install.sh
+++ webmin-install.sh
@@ -7,7 +7,7 @@ sudo apt-get upgrade
 sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 
 #Get the Webmin Package
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.890_all.deb
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.900_all.deb
 
 #Install the package
 sudo dpkg --install webmin_1.900_all.deb
